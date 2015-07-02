c256b48f57ee06f64dad0804d99818af8820bb5b
Scott Williams <scott@swilliams.me>
2015-07-02 13:52:17 -0700

    Initial Commit

---
+++ ConnectivityDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ConnectivityDemo WatchKit Extension
+//
+//  Created by Scott Williams on 7/2/15.
+//  Copyright © 2015 Scott Williams. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
